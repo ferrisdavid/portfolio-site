6c76aa07a0b7b3e52d034c2bb12dac1dcf006a64
ferrisdavid <davidferris195@gmail.com>
2023-01-28 19:17:52 -0500

    Added img width and height to tab content interface + optional bullets prop

--- interfaces/types.ts
+++ interfaces/types.ts
@@ -2,9 +2,11 @@ import { StaticImageData } from "next/image";
 
 interface ITabContent {
   img: StaticImageData;
+  width: string;
+  height: string;
   title: string;
   duration: string;
-  bullets: string[];
+  bullets?: string[];
 }
 
 export interface ITab {
