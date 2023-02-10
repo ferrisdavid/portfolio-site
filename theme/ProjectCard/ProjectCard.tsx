528b57578c5553713fe71855b648e16a02f093c2
ferrisdavid <davidferris195@gmail.com>
2023-02-09 23:11:03 -0500

    initial project showcase card component commit + card background asset

---
+++ theme/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+// Components.
+import Image, { ImageProps } from "next/image";
+
+// Assets.
+import BackdropBubbles from "../../assets/BackdropBubbles.png";
+
+// Styles.
+import styles from "./ProjectCard.module.scss";
+
+// Props.
+interface Props {
+  title: string;
+  overview: string;
+  src: ImageProps["src"];
+}
+
+/****************************************
+ * ProjectCard.tsx - Project Showcase Card Theme Component
+ ****************************************/
+const ProjectCard = ({ title, overview, src }: Props) => {
+  // Render
+  return (
+    <div className={styles.projectCard}>
+      <div className={styles.backdrop}>
+        <Image
+          layout="fill"
+          objectFit="fill"
+          objectPosition="center"
+          src={BackdropBubbles}
+          alt="Project Showcase Image"
+          height={"100%"}
+          width={"100%"}
+        />
+      </div>
+      <div className={styles.overlay}>
+        <div className={styles.projectHeadline}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.overview}>{overview}</div>
+        </div>
+        <div className={styles.projectShowcase}>
+          <Image
+            src={src}
+            alt="Project Showcase Image"
+            height={"100%"}
+            width={"100%"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
