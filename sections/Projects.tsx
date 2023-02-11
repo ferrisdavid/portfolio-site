792a9d958e042bb676866f75b7657f270370294c
ferrisdavid <davidferris195@gmail.com>
2023-02-11 17:14:24 -0500

    Added Remaining Project Showcase sections + separators

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -4,9 +4,14 @@ import { m } from "framer-motion";
 
 // Components.
 import ProjectShowcase from "../components/ProjectShowcase/ProjectShowcase";
+import ProjectSeparator from "../theme/ProjectSeparator/ProjectSeparator";
 
-// Assets.
-import TempShowcase from "../assets/GroceryStoreShowcase.png";
+// Constants.
+import {
+  GroceryStoreConfig,
+  SortingVisualizerConfig,
+  PortfolioSiteConfig,
+} from "../constants/projects";
 
 // Styles.
 import styles from "./Section.module.scss";
@@ -15,16 +20,6 @@ import styles from "./Section.module.scss";
  * Projects.tsx -
  ****************************************/
 const Projects = () => {
-  const descriptionConfig = {
-    orientation: "right",
-    descriptions: [
-      "Due to COVID-19 the simple task of shopping for daily necessities has become a stressful nightmare for millions around the world, The Grocery Store aims to bring some of the relaxation and comfort back to this process which has been lost.",
-      "The Grocery Store is an ecommerce site where users can shop their daily necessities, it implements all the common functionalities of an ecommerce site such as a shopping cart, checkout, and order history, and extends the functionality to become an expansive consumer tool with functions such as a store locator for grocery stores of the users choice, nutritional facts for common food items, as well as a built in grocery list builder to help consumers prepare for their needs.",
-    ],
-    tools: ["React", "HTML", "CSS"],
-    github: "",
-    liveLink: "",
-  };
   // Render.
   return (
     <div>
@@ -32,23 +27,25 @@ const Projects = () => {
       <div className={styles.sectionHeader}>Projects</div>
       <div style={{ padding: "25px 35px" }}>
         <ProjectShowcase
-          title="The Grocery Store"
-          overview="An E-Commerce Grocery Store Site designed for the COVID Age."
-          src={TempShowcase}
-          descriptionConfig={descriptionConfig as any}
+          title={GroceryStoreConfig.title}
+          overview={GroceryStoreConfig.overview}
+          src={GroceryStoreConfig.src}
+          descriptionConfig={GroceryStoreConfig.descriptionConfig}
         />
-        <div style={{ width: "1px", height: "20px" }} />
-        <svg>
-          <ellipse
-            cx="15"
-            cy="15"
-            rx="5"
-            ry="5"
-            stroke="red"
-            style={{ fill: "transparent", strokeWidth: "1px" }}
-          />
-        </svg>
-        <div style={{ width: "1px", height: "20px" }} />
+        <ProjectSeparator />
+        <ProjectShowcase
+          title={SortingVisualizerConfig.title}
+          overview={SortingVisualizerConfig.overview}
+          src={SortingVisualizerConfig.src}
+          descriptionConfig={SortingVisualizerConfig.descriptionConfig}
+        />
+        <ProjectSeparator orientation="right" />
+        <ProjectShowcase
+          title={PortfolioSiteConfig.title}
+          overview={PortfolioSiteConfig.overview}
+          src={PortfolioSiteConfig.src}
+          descriptionConfig={PortfolioSiteConfig.descriptionConfig}
+        />
       </div>
     </div>
   );
